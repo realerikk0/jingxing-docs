54c3968667a92350096d15448fc0dcac1c36e4c3
Eric Liu <yuanliu325@gmail.com>
2026-04-15 22:11:59 +0800

    docs: fix pages custom domain

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://doc.jingixng.uk',
+  url: 'https://doc.jingxing.uk',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -108,7 +108,7 @@ const config: Config = {
           items: [
             {
               label: '自定义域名',
-              href: 'https://doc.jingixng.uk',
+              href: 'https://doc.jingxing.uk',
             },
           ],
         },
